3f45658a1d7201bf91b470a65b62cb54cf6c738a
Leon Chen <lca163@sfu.ca>
2022-10-15 21:41:26 -0700

    fix

--- evidence_generator.py
+++ evidence_generator.py
@@ -1,6 +1,13 @@
 from dotenv import load_dotenv
 import os
 import replicate
+import numpy as np
+import requests
+import cv2
+from dotenv import load_dotenv
+import os
+import base64
+
 load_dotenv()
 
 class EvidenceGenerator:
@@ -10,4 +17,55 @@ class EvidenceGenerator:
     
     def acquire_evidence(self, theme):
         model = replicate.models.get("stability-ai/stable-diffusion")
-        return model.predict(prompt=theme)[0]
+        url = model.predict(prompt=theme)[0]
+        # api call get image
+        response = requests.post(
+            'https://api.remove.bg/v1.0/removebg',
+            data={
+                'image_url': 'https://media.istockphoto.com/photos/close-up-young-smiling-man-in-casual-clothes-posing-isolated-on-blue-picture-id1270987867?k=20&m=1270987867&s=612x612&w=0&h=lX9Y1qUxtWOa0W0Mc-SvNta00UH0-sgJQItkxfwE4uU=',
+                'size': 'auto'
+            },
+            headers={'X-Api-Key': 'rPdNmWA3gtAKbDXiTz5qEqjT'},
+        )
+        if response.status_code == requests.codes.ok:
+            with open('nobg.png', 'wb') as out:
+                out.write(response.content)
+        else:
+            print("Error:", response.status_code, response.text)
+
+        # read foreground image from api call
+        img = cv2.resize(cv2.imread('nobg.png', cv2.IMREAD_UNCHANGED), (500, 500))
+
+        resp = requests.get(url, stream=True).raw
+        image = np.asarray(bytearray(resp.read()), dtype="uint8")
+        image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+        back = cv2.resize(image, (500, 500))
+
+        # read background image
+        # back = cv2.resize(cv2.imread('background.png'), (500, 500))
+
+        # get alpha channel
+        alpha = img[:,:,3]
+        alpha = cv2.merge([alpha,alpha,alpha])
+
+        # extract bgr channels from foreground image
+        front = img[:,:,0:3]
+
+        # blend the two images using the alpha channel as controlling mask
+        result = np.where(alpha==(0,0,0), back, front)
+
+        _, buffer = cv2.imencode('.jpg', result)
+        im_bytes = buffer.tobytes()
+
+        jpg_as_text = base64.b64encode(im_bytes)
+        
+        return jpg_as_text
+        # # save result
+        # cv2.imwrite("front_back.png", result)
+
+        # # show result
+        # cv2.imshow("RESULT", result)
+        # cv2.waitKey(0)
+
+
+
--- static/js/theme.js
+++ static/js/theme.js
@@ -9,9 +9,11 @@ async function getTheme() {
 
 async function getEvidence() {
     let ele = document.getElementById("theme");
-    await fetch(window.origin + "/evidence/" + ele.value, {method: "GET"})
-            .then((res) => {
-                if (res.status == 200)
-                    return res.text();
-            }).then((res)=>document.getElementById("evidence-img").src = res);
+    const response = await fetch(window.origin + "/evidence", {method: "POST",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+              },body:ele.value});
+            const content = await response.text();
+            document.getElementById("evidence-img").src = content;
 }
